e6bd8b97dafa42e41e6207a757970196347b60dd
Nguyen Anh Tien <nguyen.anh.tien@framgia.com>
2019-11-12 08:17:21 +0700

    Add aqi level greater than 300

--- src/App.vue
+++ src/App.vue
@@ -112,11 +112,11 @@ export default {
       pm10: 0,
       time: new Date().toString(),
       timer: '',
-      levels: [50, 100, 150, 200, 300],
+      levels: [50, 100, 150, 200, 300, 500],
       last_aqis: [],
-      texts: ['Good', 'Moderate', 'Unhealthy for sensitive groups', 'Unhealthy', 'Very Unhealthy'],
-      leftClasses: ['bg-green-300', 'bg-yellow-400', 'bg-orange-400', 'bg-red-400', 'bg-purple-400'],
-      rightClasses: ['bg-green-200', 'bg-yellow-300', 'bg-orange-300', 'bg-red-300', 'bg-purple-300']
+      texts: ['Good', 'Moderate', 'Unhealthy for sensitive groups', 'Unhealthy', 'Very Unhealthy', 'Hazardous'],
+      leftClasses: ['bg-green-300', 'bg-yellow-400', 'bg-orange-400', 'bg-red-400', 'bg-purple-400', 'bg-pink-700'],
+      rightClasses: ['bg-green-200', 'bg-yellow-300', 'bg-orange-300', 'bg-red-300', 'bg-purple-300', 'bg-pink-600']
     }
   },
   created () {
